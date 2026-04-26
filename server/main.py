da291a5d411da4281cc7a76f2bd02a77b88db88e
Tal Weiss <Tweiss747@gmail.com>
2026-04-26 14:30:57 -0400

    lender route injected to fast api

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes.borrowers import borrower_routes
 from app.routes.loan_requests import loan_requests_routes
+from app.routes.lenders import lender_routes
 import os
 import uvicorn
 from dotenv import load_dotenv
@@ -10,6 +11,6 @@ PORT = int(os.environ.get("PORT", 3000))
 app = FastAPI() 
 app.include_router(borrower_routes, prefix="/api/v1/borrowers")
 app.include_router(loan_requests_routes, prefix="/api/v1/loan_requests")
-
+app.include_router(lender_routes,prefix="/api/v1/lenders")
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=True)
