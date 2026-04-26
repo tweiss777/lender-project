ef65efb10d006b53bb436aa35becb223e021e807
Tal Weiss <Tweiss747@gmail.com>
2026-04-26 17:22:03 -0400

    added fields to underwriter response.

--- server/app/Schemas/underwriter_response.py
+++ server/app/Schemas/underwriter_response.py
@@ -1,8 +1,16 @@
-from typing import Any
+from typing import Any, Optional
+from app.Schemas.underwriter_request import UnderWriterRequest
 from app.models.MatchResult import MatchStatus
-from underwriter_request import UnderWriterRequest
+
 
 class UnderwriterResponse(UnderWriterRequest):
     match_id: int
+    lender_name: str
+    program_name: Optional[str]
+    is_eligible: bool
+    match_score: float
+    status: MatchStatus
+    criteria_results: Any
+
     class Config:
         from_attributes = True
